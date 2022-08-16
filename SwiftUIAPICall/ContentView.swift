ff2e731fc9e078b7e03e9b1a51d9e27768af1907
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-08-16 16:20:01 -0400

    Initial Commit

---
+++ SwiftUIAPICall/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAPICall
+//
+//  Created by Caroline LaDouce on 8/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAPICall/SwiftUIAPICallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPICallApp.swift
+//  SwiftUIAPICall
+//
+//  Created by Caroline LaDouce on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPICallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
